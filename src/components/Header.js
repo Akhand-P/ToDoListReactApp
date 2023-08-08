afaa7da58e1e1080db3713018621252e7b77ee34
Akhand-P <sakhandpratap60@gmail.com>
2023-08-08 09:46:27 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="container" style={{backgroundColor:"black",color: "white",borderRadius:"10px"}}>
+        
+        <h1 className="text-center my-3"  href="/">ToDo List</h1>
+        
+        
+    </div>
+        
+  )
+}
